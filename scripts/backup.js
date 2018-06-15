56d092981d9934a89eef24586ccfd9a0db4c5bf6
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-06-15 19:00:02 +0200

    Add backup script

---
+++ scripts/backup.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const Logger = require('../modules/logger')
+const LOGGER = new Logger('backup')
+
+const APP_ROOT = require('app-root-path')
+const { STORAGE_PATH, STORAGE_FILENAME } = require('../config/main.config')
+
+let BACKUP_PATH = process.env.GETTYGETTER_BACKUP_PATH
+
+if (!BACKUP_PATH) {
+    LOGGER.error('No backup path specified in environment variable GETTYGETTER_BACKUP PATH. Exiting.')
+    process.exit(1)
+}
+
+if (BACKUP_PATH[BACKUP_PATH.length - 1] !== '/') {
+    BACKUP_PATH += '/'
+}
+
+let date = new Date()
+// 2018-6-15-18-45-12-234
+let datePrefix = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}-${date.getHours()}-` + 
+    `${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}`
+
+LOGGER.log(`Creating backup ${datePrefix} in ${BACKUP_PATH} ...`)
+fs.copyFileSync(STORAGE_PATH + STORAGE_FILENAME, BACKUP_PATH + datePrefix + '.' + STORAGE_FILENAME)
+fs.copyFileSync(STORAGE_PATH + STORAGE_FILENAME + '.0', BACKUP_PATH + datePrefix + '.' + STORAGE_FILENAME + '.0')
+LOGGER.log('Done.')
