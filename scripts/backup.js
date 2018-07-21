6ff02a3ae54e51772f2f7e4f9b41a07abf4e701d
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-07-22 01:50:39 +0200

    adjust backup script to copy an arbitrary number of files

--- scripts/backup.js
+++ scripts/backup.js
@@ -21,7 +21,15 @@ let date = new Date()
 let datePrefix = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}-${date.getHours()}-` + 
     `${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}`
 
+let allFiles = fs.readdirSync(STORAGE_PATH)
+let storageFiles = allFiles.filter(filename => {
+    return filename.indexOf(STORAGE_FILENAME) === 0
+})
+
 LOGGER.log(`Creating backup ${datePrefix} in ${BACKUP_PATH} ...`)
-fs.copyFileSync(STORAGE_PATH + STORAGE_FILENAME, BACKUP_PATH + datePrefix + '.' + STORAGE_FILENAME)
-fs.copyFileSync(STORAGE_PATH + STORAGE_FILENAME + '.0', BACKUP_PATH + datePrefix + '.' + STORAGE_FILENAME + '.0')
+
+for (let filename of storageFiles) {
+    LOGGER.log(`Copying file ${filename}`)
+    fs.copyFileSync(STORAGE_PATH + filename, BACKUP_PATH + datePrefix + '.' + filename)
+}
 LOGGER.log('Done.')
