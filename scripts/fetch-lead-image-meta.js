e6b7c6a7dbae5c052a822e062d9ea999d401ae79
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-08-27 20:42:03 +0200

    upgrade fetch image meta script to mongodb

--- scripts/fetch-lead-image-meta.js
+++ scripts/fetch-lead-image-meta.js
@@ -1,13 +1,16 @@
-const DB = require('../modules/db/loki')
+const Mongo = require('../modules/db/mongo')
 const Getty = require('../modules/getty/api')
 const Logger = require('../modules/logger')
 const LOGGER = new Logger('fetch-lead-image-meta')
 
-DB().then(db => {
+let mongo = new Mongo()
+mongo.init().then(() => {
     LOGGER.info('Fetching lead image metadata for the whole corpus')
-    Getty.fetchLeadImageMeta(db).then(() => {
+    Getty.fetchLeadImageMeta(mongo).then(() => {
         LOGGER.info('Done.')
     }).catch(e => {
         LOGGER.warn(`Error fetching image metadata: ${e.message}`)
+    }).then(() => {
+        process.exit()
     })
 })
